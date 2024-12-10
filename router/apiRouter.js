9c530028493d8ac0ce6687e7ac34609dbaae6bf7
daniel_colina <danielc123avila@icloud.com>
2024-12-09 21:16:50 -0500

    router ready

--- router/apiRouter.js
+++ router/apiRouter.js
@@ -1,7 +1,7 @@
 import express from "express";
-
+import ruletaRouter from "./ruletaRouter.js"
 const router = express.Router();
 
-//router.use();
-
+// Define tus rutas y asegúrate de pasar funciones de middleware válidas 
+router.use('/ruleta', ruletaRouter); 
 export default router;
