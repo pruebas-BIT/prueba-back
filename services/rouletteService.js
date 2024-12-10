341d9a7626bee5f4d11aa06cfa919f89fa5b7ed1
SamuelMunera <samuelmunera11@gmail.com>
2024-12-10 08:17:43 -0500

    edit model roulette

--- services/rouletteService.js
+++ services/rouletteService.js
@@ -1,10 +1,12 @@
  import Roulette from "../models/roulette.js";
 
- export async function createNewRoulette({Status}){
+ export async function createNewRoulette({id,Status}){
     try {
-        const newRoulette = new Roulette({Status});
+        const newRoulette = new Roulette({id, Status});
+    
         await newRoulette.save();
-        return newRoulette._id;
+        return newRoulette.
+        id;
     }catch (error){
         console.error(`error creating roulette`, error);
         throw new error(`internal server error`)
