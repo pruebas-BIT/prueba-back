887700e6f22a0e9588ca4f3e732ed2271a11bb24
SamuelMunera <samuelmunera11@gmail.com>
2024-12-09 21:47:07 -0500

    resolve routes

--- router/apiRouter.js
+++ router/apiRouter.js
@@ -1,10 +1,8 @@
 import express from "express";
-import ruletaRouter from "./ruletaRouter.js"
 import rouletteRouter from "./rouletteRouter.js";
 const router = express.Router();
 
 // Define tus rutas y asegúrate de pasar funciones de middleware válidas 
-router.use('/ruleta', ruletaRouter); 
 router.use("/roulette", rouletteRouter);
 
 export default router;
